2c6847231c8ba6049f0b3e4f5d0e381be3b74604
Ștefan Alexandru <alexandrustefan450@gmail.com>
2021-11-24 11:16:49 +0200

    new route

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -36,7 +36,7 @@ class Register extends React.Component {
       .then(user => {
         if (user) {
           this.props.loadUser(user)
-          this.props.onRouteChange('home');
+          this.props.onRouteChange('signin');
         }
       })
   }
